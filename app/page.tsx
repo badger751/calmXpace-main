794acc69a058021fa4f070a9f4f56db29f20276a
badger751 <achintyamanstar@gmail.com>
2024-12-11 17:02:08 +0530

    ninth commit

--- app/page.tsx
+++ app/page.tsx
@@ -37,6 +37,79 @@ export default async function Home() {
         <div className="col-span-3 hidden lg:block">
           <div className="space-y-4"> {/* Space for components in rows */}
             <Menu />
+            <div className="carousel rounded-box w-96">
+  <div className="carousel-item w-1/2">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+      className="w-full" />
+  </div>
+  <div className="carousel-item w-1/2">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+      className="w-full" />
+  </div>
+  <div className="carousel-item w-1/2">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
+      className="w-full" />
+  </div>
+  <div className="carousel-item w-1/2">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
+      className="w-full" />
+  </div>
+  <div className="carousel-item w-1/2">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
+      className="w-full" />
+  </div>
+  <div className="carousel-item w-1/2">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
+      className="w-full" />
+  </div>
+  <div className="carousel-item w-1/2">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+      className="w-full" />
+  </div>
+</div><div className="carousel rounded-box w-96">
+  <div className="carousel-item w-1/2">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+      className="w-full" />
+  </div>
+  <div className="carousel-item w-1/2">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+      className="w-full" />
+  </div>
+  <div className="carousel-item w-1/2">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
+      className="w-full" />
+  </div>
+  <div className="carousel-item w-1/2">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
+      className="w-full" />
+  </div>
+  <div className="carousel-item w-1/2">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
+      className="w-full" />
+  </div>
+  <div className="carousel-item w-1/2">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
+      className="w-full" />
+  </div>
+  <div className="carousel-item w-1/2">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+      className="w-full" />
+  </div>
+</div>
             {/* Future components in this column will be added below */}
           </div>
         </div>
@@ -61,6 +134,36 @@ export default async function Home() {
           <div className="space-y-4"> {/* Space for components in rows */}
             <AqiBox city={"de"} date={"9709"} aqi={0} />
             <NewsFeed title={"Headline"} newsItems={newsItems} />
+            <div className="card bg-base-100 w-96 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes"
+      className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+<div className="card bg-base-100 w-96 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes"
+      className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
             {/* Future components in this column will be added below */}
           </div>
         </div>
